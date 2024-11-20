23e097432a85c7844002876b3a06f9a49e81ac09
Yoann Mallet <yoann.mallet@eemi.com>
2024-11-20 23:40:14 +0100

    Fix(controller)!: add ContactController to handle all contact method from one controller -Created Contact Controller to handle all CRUD methods for /contact -Updated routes.json to use ContactController, it solve my method not allowed problem

--- app/src/Controllers/ContactController.php
+++ app/src/Controllers/ContactController.php
@@ -9,7 +9,18 @@ class ContactController extends AbstractController
 {
   public function process(Request $request): Response
   {
+    switch ($request->getMethod()) {
+      case 'GET':
+        return $this->getContacts($request);
+      case 'POST':
+        return $this->postContact($request);
+      default:
+        return new Response('Method not allowed', 405, ['Content-Type' => 'text/plain']);
+    }
+  }
 
+  private function postContact(Request $request): Response
+  {
     // Post contact form
     $data = json_decode($request->getPayload(), true);
 
@@ -40,5 +51,25 @@ class ContactController extends AbstractController
     file_put_contents($filepath, json_encode($formContact));
 
     return new Response(json_encode(['file' => $filename]), 201, ['Content-Type' => 'application/json']);
+  }
+  private function getContacts(Request $request): Response
+  {
+    // Get all contacts form
+    $directory = __DIR__ . '/../../var/contacts/';
+    $contacts = [];
+
+    if (is_dir($directory)) {
+      $files = scandir($directory);
+      foreach ($files as $file) {
+
+        $filePath = $directory . $file;
+
+        if (is_file($filePath)) {
+          $content_file = file_get_contents($filePath);
+          $contacts[] = json_decode($content_file, true);
+        }
+      }
+    }
+    return new Response(json_encode($contacts), 200, ['Content-Type' => 'application/json']);
   }
 }
