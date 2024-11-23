a58a0ce51f7b3ede72db24343303755116e22dd6
Morgane Dassonville <morgane.dassonville@eemi.com>
2024-11-23 21:18:37 +0100

    Retourner les formulaires de contact

--- app/src/Controllers/ContactController.php
+++ app/src/Controllers/ContactController.php
@@ -4,12 +4,17 @@ namespace App\Controllers;
 
 use App\Http\Request;
 use App\Http\Response;
+use DirectoryIterator;
 
 class ContactController extends AbstractController {
 
     public function process(Request $request): Response {
         if ($request->getMethod() === 'POST') {
             return $this->handlePost($request);
+        }
+
+        if ($request->getMethod() === 'GET') {
+            return $this->handleGet();
         }
 
         return new Response('Contact Controller');
@@ -36,5 +41,19 @@ class ContactController extends AbstractController {
         file_put_contents($filepath, json_encode($data));
 
         return new Response(json_encode(["file" => $filename]), 201);
+    }
+
+    private function handleGet(): Response {
+        $contacts = [];
+        $directory = __DIR__ . '/../../var/contacts';
+
+        foreach (new DirectoryIterator($directory) as $fileInfo) {
+            if (!$fileInfo->isDot() && $fileInfo->isFile()) {
+                $contactData = json_decode(file_get_contents($fileInfo->getPathname()), true);
+                $contacts[] = $contactData;
+            }
+        }
+
+        return new Response(json_encode($contacts), 200);
     }
 }
